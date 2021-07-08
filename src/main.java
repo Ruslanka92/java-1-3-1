36efaf905d2dcec75c398ecdce45d45740bae6a9
Ruslanka92 <85515475+Ruslanka92@users.noreply.github.com>
2021-07-08 22:11:43 +0300

    Лекция 3 задача 1 мили

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int miles;
+        int bonus = 20;
+        miles = ticketPrice / bonus;
+        System.out.println(miles);
+    }
+}
+
+
